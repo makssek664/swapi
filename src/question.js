7417feaca9ebd8658c3a83119581195785558622
makssek664 <jajcoe40@gmail.com>
2025-10-07 16:15:57 +0200

    update question handling etc

--- src/question.js
+++ src/question.js
@@ -5,24 +5,22 @@ class Question {
         this.swapi.at(irand(10, 1000)).then((elm) => {
             this.elm = elm;
             this.div.innerHTML = `
-            <h1>What's the movie?</h1>
-            <span>${this.elm.opening_crawl}</span>
+            <h1 class="question">What's the movie?</h1>
+            <span class="opening-crawl">${this.elm.opening_crawl}</span></br>
             <input class="ans">
             <button class="confirm">Confirm</button> 
             `;
             this.ans =  this.div.querySelector(".ans");
             this.conf = this.div.querySelector(".confirm");
             this.conf.addEventListener("click",  (e) => {
-                if(this.elm.title.toLowerCase().includes(this.ans.value.toLowerCase())) {
-                    this.div.innerHTML = "<h1>Congratulations!</h1>"
-                    this.div.answered = true;
+                if(this.elm.title.toLowerCase().includes(this.ans.value.toLowerCase()) && this.ans.value !== "") {
+                    this.div.innerHTML = '<h1 class="success">Congratulations!</h1>';
+                    this.answered = true;
                 } else {
-                    this.div.innerHTML = "<h1>Nope!</h1>"
-                    this.div.answered = false;
+                    this.div.innerHTML = '<h1 class="failure">Nope!</h1>';
+                    this.answered = true;
                 }
             });
-            
-            while(!this.ans) {}
 
             return;
         });
@@ -34,6 +32,7 @@ class Question {
         this.swapi = swapi;
         this.answered = true;
         this.div = document.createElement("div");
+        this.div.className = "game";
         document.body.appendChild(this.div);
     }
 }
